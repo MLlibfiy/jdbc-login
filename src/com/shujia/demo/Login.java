c48eaf7cb294aaae91c53d2b14dc758e9b4f8924
Teacher <987262086@qq.com>
2018-07-17 09:33:31 +0800

    退出登录，修改密码。防止sql注入，使用预编译sql执行器

--- src/com/shujia/demo/Login.java
+++ src/com/shujia/demo/Login.java
@@ -4,10 +4,7 @@ import com.shujia.util.DBUtils;
 import com.shujia.util.LoginUtils;
 import com.shujia.util.MD5Util;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Scanner;
 
 /**
@@ -17,7 +14,7 @@ import java.util.Scanner;
  */
 public class Login {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
 
         //判断用户名是否存在，
@@ -27,7 +24,7 @@ public class Login {
         boolean flag = true;
         while (flag) {
             System.out.println("请输入用户名：");
-            username = scanner.next();
+            username = scanner.nextLine();
             //验证用户名是否存在
             flag = !LoginUtils.verifyUsernmae(username);
             if (flag) {
@@ -39,16 +36,28 @@ public class Login {
         while (loginFlag){
             System.out.println("请输入密码：");
             //登录的时候也需要对密码加密
-            String password = MD5Util.getMD5(scanner.next());
-            String sql = "select * from user where name='" + username + "' and password='" + password + "'";
+            String password = MD5Util.getMD5(scanner.nextLine());
+            String sql = "select * from user where password=? and name=?";
+            System.out.println(sql);
             //获取连接
             Connection connection = null;
             Statement statement = null;
             try {
                 connection = DBUtils.getConnection();
                 //获取sql执行器
-                statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(sql);
+                //statement = connection.createStatement();
+                //ResultSet resultSet = statement.executeQuery(sql);
+
+                //创建预编译sql执行器
+                PreparedStatement statement1 = connection.prepareStatement(sql);
+
+                //给参数赋值
+                statement1.setObject(1,password);
+                statement1.setObject(2,username);
+
+                //执行sql
+                ResultSet resultSet = statement1.executeQuery();
+
                 //判断是否查询到数据
                 loginFlag = ! resultSet.next();
             } catch (SQLException e) {
@@ -71,7 +80,47 @@ public class Login {
             System.out.println("请选择，1：修改密码，2：退出登录");
             int i = scanner.nextInt();
 
+            if(i==2){
+                break;
+            }else  if(i==1){
+
+                boolean flag1 = true;
+                String newPassword =null;
+                while (flag1){
+                    System.out.println("请输入密码：");
+                    newPassword = scanner.next();
+                    System.out.println("请再次输入密码：");
+                    String new2Password = scanner.next();
+
+                    flag1 = ! newPassword.equals(new2Password);
+
+                    if (flag1) {
+                        System.out.println("输入密码不一致：");
+                    }
+                }
+
+                //获取连接
+                Connection connection = DBUtils.getConnection();
+                //创建执行器
+                Statement statement = connection.createStatement();
+
+                String sql = "update user set password='"+MD5Util.getMD5(newPassword)+"' where name='"+username+"'";
+
+                int i1 = statement.executeUpdate(sql);
+
+                if(i1>0){
+                    System.out.println("密码修改成功------");
+                }
+
+
+            }
+
         }
+
+    }
+
+
+    public static void modifyPassword(){
 
     }
 
--- src/com/shujia/util/LoginUtils.java
+++ src/com/shujia/util/LoginUtils.java
@@ -1,9 +1,6 @@
 package com.shujia.util;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class LoginUtils {
 
@@ -11,17 +8,34 @@ public class LoginUtils {
     /**
      * 验证用户名是否存在
      *如果用户名已存在，返回true
+     *
+     * 通过sql注入绕过用户名验证
+     * 111' or '1'='1
+     * select * from user where name='111' or '1'='1'
+     *
      */
     public static boolean verifyUsernmae(String username){
-        String selSql = "select * from user where name='"+username+"'";
+        String selSql = "select * from user where name=?";
+
+        System.out.println(selSql);
         Connection connection = null;
 
         boolean flag = false;
 
         try {
             connection = DBUtils.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(selSql);
+            //普通sql执行器，直接发送一条sql给sql服务器
+            //Statement statement = connection.createStatement();
+            //ResultSet resultSet = statement.executeQuery(selSql);
+
+            //解决sql注入。使用预编译sql执行器
+            //创建预编译sql执行器
+            PreparedStatement statement = connection.prepareStatement(selSql);
+            //给参数赋值，下标从1开始
+            statement.setObject(1,username);
+
+            //执行查询，返回结果
+            ResultSet resultSet = statement.executeQuery();
             //如果用户名已存在，返回true
             flag = resultSet.next();
         } catch (SQLException e) {
